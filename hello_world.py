f9ba41b7fd1ada4c3cb69d23ef0d934902a12e9b
Shekhar Kumar <ishekhar.k@gmail.com>
2019-01-29 22:54:35 +0100

    ngrok shall run till automatic jenkin trigger is required, trial 2

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,4 @@
 print("Hello World")
 print("Commit new change")
 print ("Jenkins triggers when commit is done")
-print("ngrok need to run till then jenkins will trigger automatically")
+print("ngrok need to run till then jenkins will trigger automatically...")
